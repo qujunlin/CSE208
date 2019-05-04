80377d5f80c80d348fa6edefc5d0204e2b6d8c64
qujunlin <qujunlinyt@163.com>
2019-05-04 17:06:40 +0800

    new v

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema ({
-  nickname: String,
-  password: String,
+  nickName: String,
+  passWord: String,
   avatarUrl: String,
   email: String,
   credits: {
